86479f6a1b19bf92eb6b98e0079d724d58f11d5d
totalian <zeki.mirza@googlemail.com>
2022-11-01 00:39:33 +0000

    Added daily database

--- src/database.js
+++ src/database.js
@@ -1,7 +1,152 @@
 export const words = [
     {
         date: new Date("2022-11-01").toDateString(),
+        startWord: "wood",
+        endWord: "fire"
+    },
+    {
+        date: new Date("2022-11-02").toDateString(),
         startWord: "fire",
-        endWord: "word"
-    }
+        endWord: "water"
+    },
+    {
+        date: new Date("2022-11-03").toDateString(),
+        startWord: "water",
+        endWord: "ocean"
+    },
+    {
+        date: new Date("2022-11-04").toDateString(),
+        startWord: "ocean",
+        endWord: "island"
+    },
+    {
+        date: new Date("2022-11-05").toDateString(),
+        startWord: "island",
+        endWord: "tree"
+    },
+    {
+        date: new Date("2022-11-06").toDateString(),
+        startWord: "tree",
+        endWord: "forest"
+    },
+    {
+        date: new Date("2022-11-07").toDateString(),
+        startWord: "forest",
+        endWord: "jungle"
+    },
+    {
+        date: new Date("2022-11-08").toDateString(),
+        startWord: "jungle",
+        endWord: "lion"
+    },
+    {
+        date: new Date("2022-11-09").toDateString(),
+        startWord: "lion",
+        endWord: "predator"
+    },
+    {
+        date: new Date("2022-11-10").toDateString(),
+        startWord: "predator",
+        endWord: "destruction"
+    },
+    {
+        date: new Date("2022-11-11").toDateString(),
+        startWord: "destruction",
+        endWord: "oblivion"
+    },
+    {
+        date: new Date("2022-11-12").toDateString(),
+        startWord: "oblivion",
+        endWord: "hope"
+    },
+    {
+        date: new Date("2022-11-13").toDateString(),
+        startWord: "hope",
+        endWord: "reconciliation"
+    },
+    {
+        date: new Date("2022-11-14").toDateString(),
+        startWord: "reconciliation",
+        endWord: "forgiveness"
+    },
+    {
+        date: new Date("2022-11-15").toDateString(),
+        startWord: "forgiveness",
+        endWord: "religion"
+    },
+    {
+        date: new Date("2022-11-16").toDateString(),
+        startWord: "religion",
+        endWord: "purity"
+    },
+    {
+        date: new Date("2022-11-17").toDateString(),
+        startWord: "purity",
+        endWord: "cleanliness"
+    },
+    {
+        date: new Date("2022-11-18").toDateString(),
+        startWord: "cleanliness",
+        endWord: "bath"
+    },
+    {
+        date: new Date("2022-11-19").toDateString(),
+        startWord: "bath",
+        endWord: "rejuvinate"
+    },
+    {
+        date: new Date("2022-11-20").toDateString(),
+        startWord: "rejuvinate",
+        endWord: "massage"
+    },
+    {
+        date: new Date("2022-11-21").toDateString(),
+        startWord: "massage",
+        endWord: "relax"
+    },
+    {
+        date: new Date("2022-11-22").toDateString(),
+        startWord: "relax",
+        endWord: "stress"
+    },
+    {
+        date: new Date("2022-11-23").toDateString(),
+        startWord: "stress",
+        endWord: "anxiety"
+    },
+    {
+        date: new Date("2022-11-24").toDateString(),
+        startWord: "anxiety",
+        endWord: "mathematics"
+    },
+    {
+        date: new Date("2022-11-25").toDateString(),
+        startWord: "mathematics",
+        endWord: "physics"
+    },
+    {
+        date: new Date("2022-11-26").toDateString(),
+        startWord: "physics",
+        endWord: "astronomy"
+    },
+    {
+        date: new Date("2022-11-27").toDateString(),
+        startWord: "astronomy",
+        endWord: "space"
+    },
+    {
+        date: new Date("2022-11-28").toDateString(),
+        startWord: "space",
+        endWord: "planet"
+    },
+    {
+        date: new Date("2022-11-29").toDateString(),
+        startWord: "planet",
+        endWord: "earth"
+    },
+    {
+        date: new Date("2022-11-30").toDateString(),
+        startWord: "earth",
+        endWord: "fire"
+    },
 ]
