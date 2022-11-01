2bd40c79febc5c61768e47d155c5bb8523064550
totalian <zeki.mirza@googlemail.com>
2022-11-01 01:04:37 +0000

    fix https

--- src/scoringFunctions/checkWord.js
+++ src/scoringFunctions/checkWord.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const checkWord = async word => {
     try {
-        await axios.get(`http://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
     } catch (err) {
         console.log('api status:', err.statusCode)
         return false
