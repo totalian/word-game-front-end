a59f09e1e725561a69033e5ceb1564c710d54451
totalian <zeki.mirza@googlemail.com>
2022-11-02 01:36:55 +0000

    styling kind of fixed

--- src/components/MoveList.js
+++ src/components/MoveList.js
@@ -4,7 +4,7 @@ import MoveCard from './MoveCard.js'
 
 const MoveList = () => {
     return (
-        <div className='flex flex-col gap-2 '>
+        <div className='flex lg:max-h-[40rem] overflow-auto pb-4 flex-col gap-2'>
             {moveSet.map((move) => <MoveCard key={move.name} name={move.name} description={move.description} example={move.example} costDescription = {move.costDescription} color={move.color} />)}
         </div>
     )
--- src/components/MoveListSection.js
+++ src/components/MoveListSection.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import MoveList from './MoveList'
 
-const MoveListSection = ({setShowMoveSection, showMoveSection}) => {
+const MoveListSection = ({ setShowMoveSection, showMoveSection }) => {
   return (
-    <div className={`lg:w-3/12 w-full bg-gray-50 flex flex-col px-2 gap-4 lg:gap-8 overflow-auto h-full ${!showMoveSection && 'hidden'} lg:flex`}>
-        <div className='uppercase font-extrabold flex justify-center mt-6'>Move List</div>
-        <button className='underline lg:hidden' onClick={() => setShowMoveSection(false)}>Hide moves</button>
+    <div className={`lg:w-3/12 w-full bg-gray-50 flex flex-col px-2 gap-4 lg:gap-8 ${!showMoveSection && 'hidden'} lg:flex`}>
+      <div className='uppercase font-extrabold flex justify-center mt-6'>Move List</div>
+      <button className='underline lg:hidden' onClick={() => setShowMoveSection(false)}>Hide moves</button>
         <MoveList />
     </div>
   )
